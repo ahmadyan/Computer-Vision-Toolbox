f8ce8163cbe9b688441c17ed9e23053949bc7800
Adel Ahmadyan <ahmadyan@gmail.com>
2019-05-24 10:42:04 -0700

    Add Yolo weights and Yolo wrapper

--- demos/object_detection_yolo3_demo.py
+++ demos/object_detection_yolo3_demo.py
@@ -1,16 +1,16 @@
 import os
 from PIL import Image
 import matplotlib.pyplot as plt
-from cortex.models.yolo.detector import YOLO, detect_video
+from cortex.models.yolo.detector import YOLO
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 cortex_base = os.path.normpath(os.path.join(dir_path, '..', 'cortex'))
-coco_classes_path = os.path.join(cortex_base, 'dataset', 'coco_classes.txt')
+coco_classes_path = os.path.join(cortex_base, 'datasets', 'coco_classes.txt')
 anchors_path = os.path.join(cortex_base, 'models', 'yolo', 'yolo_anchors.txt')
 model_path = os.path.join(cortex_base, 'models', 'yolo', 'yolo.h5')
 
-image_path = '~/data/images/00000.png'
+image_path = '/Users/ahmadyan/dataset/images/office/translation1.jpg'
 image = Image.open(image_path)
 
 options = {"classes_path": coco_classes_path, 
